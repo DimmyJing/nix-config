35094b43e0cf1931b09ef8e9c0348a0e7600bad6
Jimmy Ding <jimmyguding@gmail.com>
2025-03-24 14:05:29 +0800

    feat: cleaned up

--- modules/shared/packages.nix
+++ modules/shared/packages.nix
@@ -8,7 +8,6 @@ let
     sudo yabai --load-sa
   '';
 in with pkgs; [
-  alacritty
   jq
   yq
   ripgrep
@@ -27,34 +26,13 @@ in with pkgs; [
   opentofu
   kubernetes-helm
   colima
-  supabase-cli
   p7zip
   gh
   elixir
   imagemagick
   gleam
   erlang
-  (ruby.withPackages ( p: with p; [
-    rails
-  ]))
   (python3.withPackages ( p: with p; [
-    torch
-    polars
-    nltk
-    pillow
-    matplotlib
-    deep-translator
-    openai
-    tqdm
-    boto3
-    psycopg2
-    anthropic
-    yt-dlp
-    coverage
-    pytest
-    pytest-cov
-    aiohttp
-    pyside6
   ]))
   turso-cli
   markdownlint-cli
@@ -64,11 +42,9 @@ in with pkgs; [
   golangci-lint-langserver
   golangci-lint
   tree-sitter
-  unixODBC
   ollama
   xray
   tailwindcss
-  emscripten
   update
   clickhouse
   spotify-player
@@ -78,113 +54,20 @@ in with pkgs; [
   ffmpeg
   just
   pyright
-  glib
   doctl
   fluxcd
   natscli
   awscli
   deno
   cue
-  idris2
-  idris2Packages.idris2Lsp
   uv
   rclone
-  wasm-bindgen-cli
-  extism-cli
-  wabt
   esbuild
   typst
   wget
-  # # General packages for development and system management
-  # act
-  # aspell
-  # aspellDicts.en
-  # bash-completion
-  # bat
-  # btop
-  # coreutils
-  # difftastic
-  # du-dust
-  # gcc
-  # git-filter-repo
-  # killall
-  # neofetch
-  # openssh
-  # pandoc
-  # sqlite
-  # wget
-  # zip
-
-  # # Encryption and security tools
-  # _1password
-  # age
-  # age-plugin-yubikey
-  # gnupg
-  # libfido2
-  # pinentry
-  # yubikey-manager
-
-  # # Cloud-related tools and SDKs
-  # # docker-compose
-  # awscli2
-  # flyctl
-  # google-cloud-sdk
-  # go
-  # gopls
-  # ngrok
-  # ssm-session-manager-plugin
-  # terraform
-  # terraform-ls
-  # tflint
-
-  # # Media-related packages
-  # emacs-all-the-icons-fonts
-  # dejavu_fonts
-  # fd
-  # font-awesome
-  # glow
-  # hack-font
-  # jpegoptim
-  # noto-fonts
-  # noto-fonts-emoji
-  # pngquant
-
-  # # PHP
-  # php82
-  # php82Packages.composer
-  # php82Packages.php-cs-fixer
-  # phpunit
-
-  # # Node.js development tools
   fzf
-  # nodePackages.live-server
-  # nodePackages.nodemon
-  # nodePackages.prettier
   nodejs_23
-
-  ghc
-  haskellPackages.haskell-language-server
-  haskellPackages.cabal-install
-  # haskellPackages.ghcup
-
-  # # Source code management, Git, GitHub tools
-  # gh
-
-  # # Text and terminal utilities
-  # htop
-  # hunspell
-  # iftop
-  # jetbrains-mono
-  # jq
-  # ripgrep
-  # slack
-  # tree
-  # unrar
-  # unzip
-  # zsh-powerlevel10k
-
-  # # Python packages
-  # black
-  # python39
-  # python39Packages.virtualenv
+  # ghc
+  # haskellPackages.haskell-language-server
+  # haskellPackages.cabal-install
 ]
